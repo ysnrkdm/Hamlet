b659d70096503b9a07d1db53ed22d470b0ccaa22
ysnrkdm <euphonictech@gmail.com>
2016-05-08 00:57:20 +0900

    Fix slack messaging

--- src/SlackMessenger.hs
+++ src/SlackMessenger.hs
@@ -6,18 +6,29 @@ import Network.HTTP.Conduit
 import qualified Data.ByteString.Lazy as L
 import System.IO.Unsafe
 import Data.ByteString.Char8
+import Data.Aeson
+import Control.Applicative
 
+data Payload = Payload { test :: String } deriving Show
+instance FromJSON Payload where
+    parseJSON (Object v) = Payload <$> (v .: "text")
+
+instance ToJSON Payload where
+    toJSON (Payload text) = object [ "text" .= text ]
+
+sendMessage :: String -> IO ()
 sendMessage text = do
-  initReq <- parseUrl "https://hooks.slack.com/services/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"
+    initReq <- parseUrl "https://hooks.slack.com/services/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"
+    let payload = Payload text
 
-  let req' = initReq { secure = True } -- Turn on https
-  let req = (flip urlEncodedBody) req' [ ("text", text) ]
+    let req' = initReq { secure = True, method = "POST" } -- Turn on https
+    let req = (flip urlEncodedBody) req' [ ("payload", L.toStrict $ encode payload) ]
 
-  response <- withManager $ httpLbs req
+    response <- withManager $ httpLbs req
 
-  L.putStr $ responseBody response
+    L.putStr $ responseBody response
 
 unsafeSendMessageNow :: String -> a -> a
 unsafeSendMessageNow text expr = unsafePerformIO $ do
-    sendMessage $ pack text
+    sendMessage text
     return expr
