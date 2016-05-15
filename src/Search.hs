65d69acab22b8a70284a9a41617c1611aa24ee21
ysnrkdm <euphonictech@gmail.com>
2016-05-15 22:26:52 +0900

    Cosmetic changes

--- src/Search.hs
+++ src/Search.hs
@@ -30,13 +30,6 @@ instance Show Result where
 conv :: Move.Mv -> Result -> Result
 conv mv res = Result (-va res) (mv : pv res)
 
---minmax :: Int -> BitBoard.Bb -> Result
---minmax 0 bd = Result (Eval.eval bd) []
---minmax dep bd = maximumBy (compare `on` va) nexts
---    where
---        nexts = map next $ MoveGenerator.moveGenerationFull bd
---        next mv = conv mv . minmax (dep - 1) $ BitBoard.move bd mv
-
 moves :: (BitBoard.Bb, Result) -> [(BitBoard.Bb, Result)]
 moves (bd, result) =
     map (\ x -> (BitBoard.move bd x, conv x result)) $ MoveGenerator.moveGenerationFull bd
@@ -44,23 +37,20 @@ moves (bd, result) =
 gametree :: BitBoard.Bb -> Tree.Tree (BitBoard.Bb, Result)
 gametree p = Tree.reptree moves (p, Result 0 [])
 
---evaluate :: BitBoard.Bb -> Eval.Value
---evaluate = Tree.maximize . Tree.maptree (Eval.eval . fst) . (Tree.prune 3) . gametree
-
 -- alphabeta
 alphabeta :: (Num a, Eq a) => a -> BitBoard.Bb -> Result
--- alphabeta depth = normalizeResult . maximum . Tree.maximize' . Tree.maptree (\x -> ((Eval.eval . fst) x, snd x)) . (Tree.prune depth) . gametree
 alphabeta depth = normalizeResult . maximum . Tree.maximize' . Tree.maptree ((Eval.eval . fst) &&& snd) . Tree.prune depth . gametree
 
 alphabetaWEndSolver :: (Num a, Eq a) => a -> BitBoard.Bb -> Result
 alphabetaWEndSolver depth bb
-    | BitBoard.getNumVacant bb <= 10 =
+    | BitBoard.getNumVacant bb <= pnsDepth =
         if va pnsRes > 0 then
             SlackMessenger.unsafeSendMessageNow ("PNS Search success!: " ++ show pnsRes ++ ".") pnsRes else
             alphaRes
     | otherwise = alphaRes
     where
-        pnsRes = normalizeSolverResult (ProofNumberSearch.pnsSearch 10 bb)
+        pnsDepth = 10
+        pnsRes = normalizeSolverResult (ProofNumberSearch.pnsSearch pnsDepth bb)
         alphaRes = alphabeta depth bb
 
 normalizeSolverResult :: ProofNumberSearch.Result -> Result
